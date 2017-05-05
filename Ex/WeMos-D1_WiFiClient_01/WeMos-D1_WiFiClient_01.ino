3b767bde00b324ca2df5a8697b5d4291c3f958a4
jash.liao <jash.liao@gmail.com>
2017-05-05 21:47:52 +0800

    add Ex01 at 2017/05/05 21:47

---
+++ Ex/WeMos-D1_WiFiClient_01/WeMos-D1_WiFiClient_01.ino
@@ -0,0 +1,89 @@
+/*
+ *  This sketch sends data via HTTP GET requests to data.sparkfun.com service.
+ *
+ *  You need to get streamId and privateKey at data.sparkfun.com and paste them
+ *  below. Or just customize this script to talk to other HTTP servers.
+ *
+ */
+
+#include <ESP8266WiFi.h>
+
+const char* ssid     = "jash.net-G-WEP";
+const char* password = "jashasd700502";
+
+const char* host = "192.168.104.4";
+const char* streamId   = "....................";
+const char* privateKey = "....................";
+
+void setup() {
+  Serial.begin(9600);
+  delay(10);
+
+  // We start by connecting to a WiFi network
+
+  Serial.println();
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+  
+  WiFi.begin(ssid, password);
+  
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  Serial.println("");
+  Serial.println("WiFi connected");  
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+}
+
+int value = 0;
+
+void loop() {
+  delay(5000);
+  ++value;
+
+  Serial.print("connecting to ");
+  Serial.println(host);
+  
+  // Use WiFiClient class to create TCP connections
+  WiFiClient client;
+  const int httpPort = 8080;
+  if (!client.connect(host, httpPort)) {
+    Serial.println("connection failed");
+    return;
+  }
+  
+  // We now create a URI for the request
+  String url = "/val.php";
+  url += "?string=";
+  url += value;
+  
+  Serial.print("Requesting URL: ");
+  Serial.println(url);
+  delay(5000);
+  // This will send the request to the server
+  client.print(String("GET ") + url + " HTTP/1.1\r\n" +
+               "Host: " + host + "\r\n" + 
+               "Connection: close\r\n\r\n");
+  unsigned long timeout = millis();
+  while (client.available() == 0) {
+    if (millis() - timeout > 5000) {
+      Serial.println(">>> Client Timeout !");
+      client.stop();
+      return;
+    }
+  }
+  
+  // Read all the lines of the reply from server and print them to Serial
+  while(client.available()){
+    String line = client.readStringUntil('\r');
+    Serial.print(line);
+  }
+  
+  Serial.println();
+  Serial.println("closing connection");
+}
+
---
+++ Ex/WeMos-D1_WiFiClient_01/php_code/val.php
@@ -0,0 +1,8 @@
+﻿<?php
+	$val = $_GET['string'];
+	$bf = fopen("test.txt","a+");
+	fwrite ($bf,$val."\r\n");
+	fclose($bf);
+	echo "get data=";
+	echo $val;
+?>
